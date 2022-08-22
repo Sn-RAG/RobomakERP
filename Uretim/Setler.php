e3ca92e68142416c965160d2297f410912590b2a
Resul Ahmet GÜNAL <resulahmetgunal@gmail.com>
2022-08-22 08:41:03 +0300

    22.08.2022

--- Uretim/Setler.php
+++ Uretim/Setler.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 $page = "Devam eden";
 require __DIR__ . '/../controller/Header.php';
 require __DIR__ . '/../controller/Db.php';
@@ -24,6 +25,7 @@ require __DIR__ . '/../controller/Sil.php';
                     <thead>
                         <tr>
                             <th>#</th>
+                            <th>&nbsp</th>
                             <th>Set ID</th>
                             <th><?= isset($_GET["Sec"]) ? "" : "Set" ?></th>
                             <th><?= isset($_GET["Sec"]) ? "Set" : "Durum" ?></th>
@@ -32,26 +34,34 @@ require __DIR__ . '/../controller/Sil.php';
                     </thead>
                     <tbody>
                         <?php
-                        $sorgu = $baglanti->query('SELECT Set_ID, SetAdi, Set_icerik_ID FROM view_uretim_setler INNER JOIN set_icerik ON view_uretim_setler.Set_Urun_ID = set_icerik.Set_Urun_ID');
-                        foreach ($sorgu as $sonuc) {
-                            $id = $sonuc['Set_icerik_ID'];
-                            $SetID = $sonuc['Set_ID'];
-                            $SetAdi = $sonuc['SetAdi'];
+                        $sorgu = $baglanti->query('SELECT Set_ID, SetAdi, Set_icerik_ID, Durum FROM view_uretim_setler INNER JOIN set_icerik ON view_uretim_setler.Set_Urun_ID = set_icerik.Set_Urun_ID');
+                        foreach ($sorgu as $s) {
+                            $id = $s['Set_icerik_ID'];
+                            $SetID = $s['Set_ID'];
+                            $SetAdi = $s['SetAdi'];
+                            $Durum = $s['Durum'];
                             require __DIR__ . '/SetKontrol/Yuzde.php';
-                            @$Yukleme = $SetYuzde == 100 ? "Yükleme Bekliyor" : $SetYuzde . "%";
+                            @$Yukleme = $Durum == 1 ? "Yüklendi" : ($SetYuzde == 100 ? "Yükleme Bekliyor" : $SetYuzde . "%");
                             @$Yuzde = $SetYuzde <= 5 ? 5 : $SetYuzde;
 
                         ?>
                             <tr>
                                 <td><?= $id ?></td>
+                                <th><?= $Durum ?></th>
                                 <td><?= $SetID ?></td>
                                 <td><a href="SetKontrol/SetKontrol.php?SetAdi=<?= $SetAdi ?>&Set_ID=<?= $SetID ?>" class="btn btn-light form-control mt-1 fw-bold"><?= $SetAdi ?></a></td>
                                 <td>
                                     <div class="progress mt-2" style="height: 25px;">
-                                        <div class="progress-bar" role="progressbar" style="width: <?= $Yuzde ?>%"><?= $Yukleme ?></div>
+                                        <div class="progress-bar <?= $Durum == 1 ? "bg-success" : "" ?>" role="progressbar" style="width: <?= $Yuzde ?>%"><?= $Yukleme ?></div>
                                     </div>
                                 </td>
-                                <td class="text-center"><a href='Setler.php?UretimSetlerSil=<?= $id ?>&Set_ID=<?= $SetID ?>' class='bi-trash btn btn-danger'></a></td>
+                                <td class="text-center">
+                                    <div class="btn-group">
+                                        <a href='Setler.php?YTamam&id=<?= $SetID ?>' class='bi-check btn btn-success me-2'></a>
+                                        <a href='Setler.php?Yiptal&id=<?= $SetID ?>' class='bi-dash btn btn-warning me-2'></a>
+                                        <a href='Setler.php?UretimSetlerSil=<?= $id ?>&Set_ID=<?= $SetID ?>' class='bi-trash btn btn-danger'></a>
+                                    </div>
+                                </td>
                             </tr>
                         <?php } ?>
                     </tbody>
@@ -63,9 +73,10 @@ require __DIR__ . '/../controller/Sil.php';
 <script>
     $('.datatablem').DataTable({
         responsive: true,
+        order: [1, 'asc'],
         columnDefs: [{
                 visible: false,
-                targets: [0, 1]
+                targets: [0, 1, 2]
             },
             {
                 targets: 4,
@@ -88,6 +99,17 @@ require __DIR__ . '/../controller/Sil.php';
     });
 </script>
 <?php
-ob_end_flush();
 require __DIR__ . '/../controller/Footer.php';
+
+@$Setid = strip_tags(htmlspecialchars(trim($_GET['id'])));
+if (isset($_GET['YTamam'])) {
+    $baglanti->query("UPDATE `set` SET Durum=1 WHERE Set_ID =" . $Setid);
+    logtut($Kullanici, "tyu sildi.");
+    header("location:Setler.php");
+} elseif (isset($_GET['Yiptal'])) {
+    $baglanti->query("UPDATE `set` SET Durum=0 WHERE Set_ID =" . $Setid);
+    logtut($Kullanici, "Set sildi.");
+    header("location:Setler.php");
+}
+ob_end_flush();
 ?>
